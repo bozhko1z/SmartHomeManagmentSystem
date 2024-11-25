f787d06d1527d309cb1b7aae1dca9230539c4137
bozhko1z <133799654+bozhko1z@users.noreply.github.com>
2024-11-25 20:32:23 +0200

    removed unnecesery things from login

--- SmartHomeManagmentSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SmartHomeManagmentSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -40,12 +40,6 @@ namespace SmartHomeManagmentSystem.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string ReturnUrl { get; set; }
 
         /// <summary>
@@ -97,7 +91,7 @@ namespace SmartHomeManagmentSystem.Areas.Identity.Pages.Account
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            
 
             ReturnUrl = returnUrl;
         }
@@ -106,7 +100,6 @@ namespace SmartHomeManagmentSystem.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
             {
