c075a32fa15b59f4d4c5c53a256d08b5d6ea0d1c
bozhko1z <133799654+bozhko1z@users.noreply.github.com>
2024-11-26 19:25:14 +0200

    fixed somw peoblems with the watchlist html view

---
+++ SmartHome.Web.ViewModels/DeviceWatchList/UserWatchListViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SmartHome.Web.ViewModels.DeviceWatchList
+{
+    public class UserWatchListViewModel
+    {
+        public string DeviceId { get; set; } = null!;
+
+        public string DeviceName { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public bool Status { get; set; }
+    }
+}
