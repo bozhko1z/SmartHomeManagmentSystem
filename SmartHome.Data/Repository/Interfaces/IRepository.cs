28c0fcdb282845d377a244c6090acf8aba71d893
bozhko1z <133799654+bozhko1z@users.noreply.github.com>
2024-12-01 12:34:39 +0200

    started implementing repository pattern

---
+++ SmartHome.Data/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Data.Repository.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(Guid id);
+
+        T GetByIdAsync(Guid id);
+    }
+}
