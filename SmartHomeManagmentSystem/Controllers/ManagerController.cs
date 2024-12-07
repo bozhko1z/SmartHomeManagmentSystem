b69e2c9a028d14d281617c9ac541a06860372090
bozhko1z <133799654+bozhko1z@users.noreply.github.com>
2024-12-07 18:45:45 +0200

    created manager for crud operations

---
+++ SmartHomeManagmentSystem/Controllers/ManagerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHome.Data;
+using SmartHome.Services.Data.Interfaces;
+using SmartHome.Web.ViewModels.Device;
+using SmartHome.Web.ViewModels.Room;
+
+namespace SmartHomeManagmentSystem.Controllers
+{
+    public class ManagerController : BaseController
+    {
+        private readonly IRoomService roomService;
+        private readonly IDeviceService deviceService;
+
+        public ManagerController(IDeviceService deviceService, IRoomService roomService)
+        {
+            this.deviceService = deviceService;
+            this.roomService = roomService;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageDevices()
+        {
+            IEnumerable<AllDevicesViewModel> allDevices = await this.deviceService.GetAllDevicesAsync();
+
+            return View(allDevices);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageRoom()
+        {
+            IEnumerable<RoomIndexViewModel> rooms = await this.roomService.GetAllRoomsAsync();
+
+
+            return View(rooms);
+        }
+    }
+}
