4592c1e47b35e28c335fca2cd8af9f3c9e1aecf3
bozhko1z <133799654+bozhko1z@users.noreply.github.com>
2024-12-08 22:55:04 +0200

    created the view for managing users

---
+++ SmartHomeManagmentSystem/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartHome.Data.Models;
+using SmartHomeManagmentSystem.Areas.Admin.ViewModels;
+
+namespace SmartHomeManagmentSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserManagementController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = this.userManager.Users.ToList();
+            var userViewModel = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                userViewModel.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Roles = roles.ToList()
+                });
+            }
+            return View(userViewModel);
+        }
+    }
+}
---
+++ SmartHomeManagmentSystem/Areas/Admin/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartHomeManagmentSystem.Areas.Admin.ViewModels
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
